41aa2cee15df62788ed6dcfdecd7da516f0f6c4f
Zhurp2020 <18916677082@126.com>
2020-01-28 17:49:11 +0800

    AC

---
+++ 1046 划拳/1046.py
@@ -0,0 +1,20 @@
+n = int(input())
+def whowin(aHan,aHua,bHan,bHua) :
+    if aHua == aHan + bHan == bHua:
+        return None
+    elif bHua == aHan + bHan :
+        return 'B'
+    elif aHua == aHan + bHan:
+        return 'A'
+counta = countb = 0
+
+for i in range(n) :
+    nums = list(map(int,input().split()))
+    result = whowin(nums[0],nums[1],nums[2],nums[3])
+    print(result)
+    if result == 'A' :
+        countb += 1
+    if result == 'B' :
+        counta += 1
+    
+print(counta,countb)
