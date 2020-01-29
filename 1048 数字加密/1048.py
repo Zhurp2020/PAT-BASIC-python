3ce5156917339454b33e1cea21be13da4917aeb5
Zhurp2020 <18916677082@126.com>
2020-01-29 16:12:28 +0800

    AC

--- 1048 数字加密/1048.py
+++ 1048 数字加密/1048.py
@@ -2,7 +2,10 @@ a, b = input().split()
 NumDict = {10:'J',11:'Q',12:'K'}
 res = ''
 
-for i in range(1,min(len(a),len(b))+1) :
+if len(b) < len(a) :
+    for i in range(len(a)-len(b)) :
+        b = b.join(['0',''])
+for i in range(1,len(a)+1) :
     if i % 2 != 0 :
         temp = (int(a[-i]) + int(b[-i])) % 13
         if temp in NumDict :
@@ -13,6 +16,6 @@ for i in range(1,min(len(a),len(b))+1) :
             temp += 10
     res = res.join([str(temp),''])
 
-if len(b)-len(a) > 0 :
+if len(b)>len(a)  :
     res = res.join([b[0:len(b)-len(a)],''])
 print(res)
